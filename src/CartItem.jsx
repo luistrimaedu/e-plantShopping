805c604a311ea1f7290d9b6fc688e30098bca637
luistrimaedu <luistrima.edu@gmail.com>
2024-12-08 12:37:12 +0100

    project ended

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,35 +1,64 @@
-import React from 'react';
+import React from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach(item => {
+      let numericPrice = Number(item.cost.replace(/[^\d.-]/g, "")); //Convert item cost to number
+      totalCost += numericPrice * item.quantity;
+    });
+    console.log("cart", cart);
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    console.log('called continue desde carrito');
+    onContinueShopping();
   };
 
-
-
   const handleIncrement = (item) => {
+    console.log("handleIncrement called");
+    console.log("item: ", item);
+    let newQuantity = item.quantity + 1;
+    let name = item.name;
+    //newQuantity ++;
+    dispatch(updateQuantity({name, quantity: newQuantity}));
+    console.log(newQuantity);
   };
 
   const handleDecrement = (item) => {
-   
+    let name = item.name;
+
+    if (item.quantity > 1) {
+      let newQuantity = item.quantity - 1;
+      dispatch(updateQuantity({name, quantity: newQuantity}));
+    } else {
+      dispatch(removeItem(item));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let numericPrice = Number(item.cost.replace(/[^\d.-]/g, "")); //Convert item cost to number
+    let itemCost = numericPrice * item.quantity;
+    return itemCost;
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
   return (
@@ -57,7 +86,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,52 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+        console.log("Added an existing product");
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+        console.log("new product");
+      }
     },
     removeItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const index = state.items.findIndex(item => item.name === name);
+      console.log("state from remove: ",state.items);
+      console.log("index: :", index)
+      //state.items = state.items.filter(item => item.name !== action.payload);
+      if (index !== -1) {
+        state.items.splice(index, 1);;
+      } else {
+        console.log(`${item.name} no está en el carrito`);
+      }
+
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload;
+      console.log("name update: " ,name);
+      const itemToUpdate = state.items.find(item => item.name === name);
+      console.log("itemToUpdate: ",itemToUpdate);
+      console.log("payload: ",action.payload);
+      //console.log("item.name: ",item.name);
+      
+      if (itemToUpdate){
+        itemToUpdate.quantity = quantity;
+      }
+      
+      //const existingItem = state.items.find(item => item.name === name);
+      /*const updatedItems = state.items.map(item => {
+        if (item.name === name) {
+          // Si encontramos el ítem, actualizamos su cantidad
+          return {
+            ...item,  // Copiamos las demás propiedades del ítem
+            quantity: quantity  // Actualizamos solo la propiedad quantity
+          };
+        }
+        return item;  // Si no es el ítem que buscamos, lo dejamos igual
+      });*/
     },
   },
 });
