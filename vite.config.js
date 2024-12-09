0c307a7c8d1466932d29e23d0a1e975d18c10090
luistrimaedu <luistrima.edu@gmail.com>
2024-12-09 20:34:18 +0100

    publish gh-pages correct

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   //base: "/shoppingreact",
-  base: "https://github.com/luistrimaedu/e-plantShopping",
+  base: "/e-plantShopping",
   plugins: [react()],
 })
